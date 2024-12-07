9d0ab51bb6ae3d2097a681cce274f6f676955687
Hugo Norte <hugonorte@gmail.com>
2024-12-07 01:16:32 -0300

    database constatn

---
+++ pdv/database.py
@@ -0,0 +1,22 @@
+"""Copyright (c) 2024."""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from pdv.settings import Settings
+
+"""Módulo de acesso ao banco de dados. """
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+engine = create_engine(Settings().DATABASE_URL)
+
+
+def get_session() -> Session:
+    """Retorna uma sessão do banco de dados.
+
+    Yields:
+        Session: Uma sessão do banco de dados.
+
+    """
+    with Session(engine) as session:
+        yield session
